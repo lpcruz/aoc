33cc377b0e9db1ff430b57a47592094dea4e4e4a
Laurence Cruz <lcruz@squarespace.com>
2020-12-02 09:56:26 -0500

    :construction: (progress): Day 2: TDD - adding simple validation helper and test

--- 2020/2/__tests__/util.unit.js
+++ 2020/2/__tests__/util.unit.js
@@ -1,9 +1,16 @@
-const { getOccurrence } = require('../src/util');
+const { getOccurrence, validate } = require('../src/util');
 
 describe('Day 2 utilities', () => {
   describe('getOccurrence', () => {
     it('should return the length of times a target value appears in an array', () => {
       expect(getOccurrence(['a', 'b', 'c', 'd', 'd'], 'd')).toEqual(2);
     });
-  })
+  });
+
+  describe('validate', () => {
+    it('should return true when given value fulfills min limit', () => {
+      const testLimitData = { min: 1, max: 9 };
+      expect(validate(testLimitData, 1)).toEqual(true);
+    });
+  });
 })
--- 2020/2/src/util.js
+++ 2020/2/src/util.js
@@ -7,6 +7,28 @@ function getOccurrence(array, value) {
   return array.filter((v) => (v === value)).length;
 }
 
+/**
+ * 
+ * @param {Object} limits - Contains the max and min thresholds 
+ * @param {*} given
+ */
+function validate(limits, value) {
+  if (!limits) {
+    throw new Error(`You must pass in a max and min limit. Received ${limits}`);
+  }
+  if (value === limits.max) {
+    console.log(`🎉 ${value} fulfills expected max limit of ${limits.max}`);
+    return true;
+  }
+  if (value === limits.min) {
+    console.log(`🎉 ${value} fulfills expected min limit of ${limits.min}`);
+    return true;
+  }
+  console.error(`👎 ${value} did not fulfill either the max limit of ${limits.max} or min limit of ${limits.min}`);
+  return false;
+}
+
 module.exports = {
-  getOccurrence
+  getOccurrence,
+  validate
 }
